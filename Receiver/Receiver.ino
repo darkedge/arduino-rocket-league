a4acefe9e58c4e89637e9d2c2fd72f61a070b45c
Marco Jonkers <contact@marcojonkers.com>
2019-02-02 20:18:38 +0100

    Magic value check, maximize send rate on transmitter, one second linger time on detection

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -2,6 +2,9 @@
 #include <SPI.h>
 
 #define INPUT_PIN (7)
+#define OUTPUT_PIN (8)
+#define MAGIC_VALUE (0x504c414e) // "PLAN" (PlatteLAN)
+#define LINGER_MS (1000) // 1 second linger time when detecting the ball
 
 // Garden side                                                         
 //         +------------------------------------------------+          
@@ -20,6 +23,7 @@
 
 
 static RH_ASK s_Driver(2000, INPUT_PIN);
+static uint16_t s_Linger; // Current linger time
 
 /**
  * Initialization.
@@ -35,24 +39,64 @@ void setup()
 
 
 /**
+ * Executed every millisecond.
+ */
+static void DoMillisecond()
+{
+  if (s_Linger == 1)
+  {
+    Serial.println("Output pin low.");
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  
+  // Keep setting pin every millisecond
+  if (s_Linger > 0)
+  {
+    digitalWrite(OUTPUT_PIN, HIGH);
+    s_Linger--;
+  } else if (s_Linger == 0)
+  {
+    digitalWrite(OUTPUT_PIN, LOW);
+  }
+
+  // Print a heartbeat just to make sure we're connected.
+  static uint16_t heartBeatTicks;
+  if(heartBeatTicks++ == 5000) // Good enough for the Arduino Nano.
+  {
+    Serial.println("Heartbeat");
+    heartBeatTicks = 0;
+  }
+}
+
+/**
  * Main loop.
  */
 void loop()
 {
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-  uint8_t buflen = sizeof(buf);
+  //uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint32_t plan;
+  uint8_t buflen = sizeof(plan);
  
-  if (s_Driver.recv(buf, &buflen)) {
-    s_Driver.printBuffer("Received: ", buf, buflen);
+  if (s_Driver.recv((uint8_t*)&plan, &buflen)) {
+    //s_Driver.printBuffer("Received: ", buf, buflen);
     //buf[buflen] = '\0';
     //Serial.println((const char*)buf);
+    if (plan == MAGIC_VALUE)
+    {
+      if (s_Linger == 0)
+      {
+        Serial.println("Output pin high.");
+      }
+      s_Linger = LINGER_MS; // Reset linger period
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
   }
 
-  // Print a heartbeat just to make sure we're connected.
-  static uint32_t heartBeatTicks;
-  if(heartBeatTicks++ == 100000) // Good enough for the Arduino Nano.
+  static unsigned long s_Milliseconds;
+  unsigned long ms = millis();
+  if (ms != s_Milliseconds)
   {
-    Serial.println("Heartbeat");
-    heartBeatTicks = 0;
+    DoMillisecond();
+    s_Milliseconds = ms;
   }
 }
